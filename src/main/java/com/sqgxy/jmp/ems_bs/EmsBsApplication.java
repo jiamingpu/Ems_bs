c5b9b0551e2f731f233c56f2f25051b346258865
jmp <157954764@qq.com>
2020-11-30 13:30:27 +0800

    commit Message

---
+++ src/main/java/com/sqgxy/jmp/ems_bs/EmsBsApplication.java
@@ -0,0 +1,13 @@
+package com.sqgxy.jmp.ems_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqgxy/jmp/ems_bs/EmsBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqgxy.jmp.ems_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
